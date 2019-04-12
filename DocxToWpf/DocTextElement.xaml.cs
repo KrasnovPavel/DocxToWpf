d482782ec0537ef9b13e40a22e89bbccfd4843e1
Pavel Krasnov <krasnovpavel0@gmail.com>
2019-04-12 16:07:27 +0300

    Добавлены коментарии для проверки с помощью PVS-Studio

--- DocxToWpf/DocTextElement.xaml.cs
+++ DocxToWpf/DocTextElement.xaml.cs
@@ -1,4 +1,7 @@
 using System.Windows.Controls;
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Xml.Linq;
